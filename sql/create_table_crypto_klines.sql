49039fee7966b8808418cba26206e8587f012362
lpdgrl <leopard.grellii@gmail.com>
2025-11-22 20:17:17 +0500

    Add task in the TODO.MD and change unique in table crypto_klines

--- sql/create_table_crypto_klines.sql
+++ sql/create_table_crypto_klines.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS crypto_klines (
     trades_count INTEGER NOT NULL,
     taker_buy_base_volume NUMERIC(24,10) NOT NULL,
     taker_buy_quote_volume NUMERIC(24,10) NOT NULL,
-    UNIQUE (symbol, open_time)
+    UNIQUE (symbol, open_time, close_time)
 )
